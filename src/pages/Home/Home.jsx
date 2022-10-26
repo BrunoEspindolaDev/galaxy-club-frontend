85fa78807330f5f8811d3a35ff99ec5c8bc1e4df
BrunoEspindola <brunoespindola.work@gmail.com>
2022-10-26 00:10:35 -0300

    Creating connection with server

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -11,91 +11,92 @@ import {
   Tab,
   TabPanel,
   useMediaQuery,
+  useToast,
+  Spinner,
 } from "@chakra-ui/react";
 import Navbar from "components/Navbar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Places = () => {
+const Items = ({ title, items }) => {
   const navigate = useNavigate();
   return (
     <Flex as="section" direction="column" rowGap={5}>
-      <Heading fontSize="sm" display={["none", "block"]}>
-        Áreas do Clube
-      </Heading>
-      <Stack direction={["column", "row"]} align="center" wrap="wrap" columnGap={7} rowGap={12}>
-        {Array.from({ length: 4 }).map((_, i) => (
-          <Flex
-            key={i}
-            w="100%"
-            flex={1}
-            transition="0.3s"
-            direction="column"
-            rowGap={2}
-            cursor="pointer"
-            _hover={{ opacity: 0.5 }}
-            onClick={() => navigate("reservation")}
-          >
-            <Box
-              mb={2}
-              h="240px"
-              rounded="md"
-              backgroundImage="url(https://images.pexels.com/photos/46798/the-ball-stadion-football-the-pitch-46798.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)"
-              backgroundSize="cover"
-              backgroundPosition="center"
-              backgroundRepeat="no-repeat"
-            />
-            <Heading fontSize="sm">Campo de Futebol</Heading>
-            <Text fontSize="xs" color="whiteAlpha.700">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            </Text>
-          </Flex>
-        ))}
-      </Stack>
-    </Flex>
-  );
-};
-
-const Equipaments = () => {
-  const navigate = useNavigate();
-  return (
-    <Flex as="section" direction="column" rowGap={5}>
-      <Heading fontSize="sm" display={["none", "block"]}>
-        Áreas do Clube
-      </Heading>
-      <Stack direction={["column", "row"]} align="center" wrap="wrap" columnGap={7} rowGap={12}>
-        {Array.from({ length: 4 }).map((_, i) => (
-          <Flex
-            key={i}
-            w="100%"
-            flex={1}
-            transition="0.3s"
-            direction="column"
-            rowGap={2}
-            cursor="pointer"
-            _hover={{ opacity: 0.5 }}
-            onClick={() => navigate("reservation")}
-          >
-            <Box
-              mb={2}
-              h="240px"
-              rounded="md"
-              backgroundImage="url(https://images.pexels.com/photos/901974/pexels-photo-901974.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)"
-              backgroundSize="cover"
-              backgroundPosition="center"
-              backgroundRepeat="no-repeat"
-            />
-            <Heading fontSize="sm">Telescópio</Heading>
-            <Text fontSize="xs" color="whiteAlpha.700">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            </Text>
-          </Flex>
-        ))}
-      </Stack>
+      {items && (
+        <Flex direction="column" rowGap={8}>
+          <Heading fontSize="sm" display={["none", "block"]}>
+            {title}
+          </Heading>
+          <Stack direction={["column", "row"]} align="center" wrap="wrap" columnGap={7} rowGap={12}>
+            {items.map(({ id, attributes }) => (
+              <Flex
+                key={id}
+                w="100%"
+                maxW="326px"
+                transition="0.3s"
+                direction="column"
+                rowGap={2}
+                cursor="pointer"
+                _hover={{ opacity: 0.5 }}
+                onClick={() => navigate("reservation")}
+              >
+                <Box
+                  mb={2}
+                  h="260px"
+                  rounded="md"
+                  backgroundImage={`url(https://artsmidnorthcoast.com/wp-content/uploads/2014/05/no-image-available-icon-6.png)`}
+                  backgroundSize="cover"
+                  backgroundPosition="center"
+                  backgroundRepeat="no-repeat"
+                />
+                <Heading fontSize="sm">{attributes.name}</Heading>
+                <Text fontSize="xs" color="whiteAlpha.700">
+                  {attributes.description}
+                </Text>
+              </Flex>
+            ))}
+          </Stack>
+        </Flex>
+      )}
     </Flex>
   );
 };
 
 const Home = () => {
   const [isMobile] = useMediaQuery("(max-width: 563px)");
+
+  const [places, setPlaces] = useState(null);
+  const [equipaments, setEquipaments] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const toast = useToast();
+
+  useEffect(() => {
+    if (!places && !equipaments) {
+      setIsLoading(true);
+
+      const handleError = () => {
+        return toast({
+          status: "error",
+          description: "Comportamento inesperado, tente novamente mais tarde",
+        });
+      };
+
+      axios
+        .all([
+          axios.get("http://localhost:1337/api/places"),
+          axios.get("http://localhost:1337/api/equipaments"),
+        ])
+        .then(
+          axios.spread((placesRes, equipamentsRes) => {
+            console.log("Res: ", placesRes);
+            setPlaces(placesRes.data.data);
+            setEquipaments(equipamentsRes.data.data);
+          })
+        )
+        .catch(handleError)
+        .finally(() => setIsLoading(false));
+    }
+  }, [places, equipaments]);
 
   return (
     <Flex minH="100vh" direction="column" bg="#161618">
@@ -110,7 +111,7 @@ const Home = () => {
           rounded="xl"
           rowGap="82px"
           pt={[0, 14]}
-          pb={[5, 15]}
+          pb={[5, 14]}
         >
           {isMobile && (
             <Tabs colorScheme="purple">
@@ -124,19 +125,25 @@ const Home = () => {
               </TabList>
               <TabPanels py={8} px={0}>
                 <TabPanel px={0}>
-                  <Places />
-                </TabPanel>
-                <TabPanel px={0}>
-                  <Equipaments />
+                  <Items title="Áreas do Clube" items={places} />
+                  <Items title="Equipamentos" items={equipaments} />
                 </TabPanel>
               </TabPanels>
             </Tabs>
           )}
           {!isMobile && (
             <>
-              <Places />
-              <Equipaments />
+              <Items title="Áreas do Clube" items={places} />
+              <Items title="Equipamentos" items={equipaments} />
             </>
+          )}
+          {isLoading && (
+            <Flex direction="column" align="center" rowGap={4}>
+              <Spinner />
+              <Text as="h5" fontSize="sm">
+                Carregando...
+              </Text>
+            </Flex>
           )}
         </Flex>
       </Flex>
