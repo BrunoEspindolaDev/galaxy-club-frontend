a845d2793479a49ee6eca6d64e4d6d94f0766fef
BrunoEspindola <brunoespindola.work@gmail.com>
2022-12-11 16:06:47 -0300

    Fixed useSubscription hook

--- src/hooks/useSubscription.js
+++ src/hooks/useSubscription.js
@@ -3,13 +3,11 @@ import moment from "moment";
 const useSubscription = () => {
   const storagedUser = JSON.parse(localStorage.getItem("user"));
 
-  const today = moment();
   const subscriptionStart = moment(storagedUser.subscriptionStart);
   const subscriptionEnd = subscriptionStart.add(30, "days");
-  const subscriptionRemainingInDays = subscriptionEnd.diff(today, "days", false);
+  const subscriptionRemainingInDays = subscriptionEnd.diff(moment(), "days", false);
 
   return {
-    today,
     subscriptionStart,
     subscriptionEnd,
     subscriptionRemainingInDays,
